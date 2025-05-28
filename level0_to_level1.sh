e4622410634bf0578d976a50ba50ebcc5a6245fd
Shubha Sandesh Sharma Neupane <131187358+indivshubha@users.noreply.github.com>
2025-05-28 12:53:21 +0545

    Create level0_to_level1.sh
    Bandit Level 0 to Level 1.

---
+++ level0_to_level1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Password for bandit0: 
+#     bandit0 : you already have access to this level by default
+# Password for bandit1 (found in readme file): 
+#     boJ9jbbUNNfktd78OOpsqOltutMc3MY1
+
+ssh bandit0@bandit.labs.overthewire.org -p 2220
+
+#Inside SSH:
+cat readme
+exit
