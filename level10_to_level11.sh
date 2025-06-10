daba3d0cfff65e8080d740e90fb6ede6b1c0902a
Shubha Sandesh Neupane <131187358+indivshubha@users.noreply.github.com>
2025-06-10 19:50:43 +0545

    Removed passsword

--- level10_to_level11.sh
+++ level10_to_level11.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# bandit10 password: FGUW5ilLVJrxX9kMYMmlN4MgbpfMiqey
-# bandit11 password: dtR173fZKb0RRsDFSGsg2RWnpNVj3qRr
 
 ssh bandit10@bandit.labs.overthewire.org -p 2220
 # Inside SSH:
